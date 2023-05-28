03f093c8c6c844137ba0815e78f5200204ebdefd
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-28 18:14:03 +0530

    Add files via upload

---
+++ camillia/modules/animequote.py
@@ -0,0 +1,51 @@
+
+import requests 
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+#NAME -> YOUR BOTS FILE NAME 
+from camellia import pbot as app
+
+
+def call_back_in_filter(data):
+    return filters.create(lambda flt, _, query: flt.data in query.data,
+                          data=data)
+
+
+
+@app.on_callback_query(call_back_in_filter('quote'))
+async def callback_quotek(_, query):
+    if query.data.split(":")[1] == "change":
+        #         query.message.delete()
+        kk = requests.get('https://animechan.vercel.app/api/random').json()
+        anime = kk['anime']
+        quote = kk['quote']
+        character = kk['character']
+        caption = f"""
+** ANIME:** `{anime}`
+
+** CHARACTER:** `{character}`
+
+** QUOTE:** `{quote}`"""
+        await query.message.edit(caption,
+                           reply_markup=InlineKeyboardMarkup([
+                               [
+                                   InlineKeyboardButton(
+                                       "CHANGE 🔄", callback_data="quotek:change")
+                               ],
+                           ]))
+
+
+@app.on_message(filters.command('aniquote'), group=91)
+async def quote(_, message):
+    kk = requests.get('https://animechan.vercel.app/api/random').json()
+    anime = kk['anime']
+    quote = kk['quote']
+    character = kk['character']
+    caption = f"""
+** ANIME:** `{anime}`
+
+** CHARACTER:** `{character}`
+
+** QUOTE:** `{quote}`"""
+    await message.reply(caption, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("CHANGE 🔄", callback_data="quotek:change")]]))")
